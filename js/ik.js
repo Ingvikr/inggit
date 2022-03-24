d41e6d0c4edc4f89f46f073cc418e2ef602be04a
Ingvikr <igoranatoly870@gmail.com>
2022-03-24 21:42:57 +0200

    sdfsdfsd

---
+++ js/ik.js
@@ -0,0 +1,5 @@
+var lk_1 = [1, 2, 3, 4, 5];
+if(lk_1[0] != lk_1[1] && lk_1[0] < lk_1[2]) {
+    var lk_2 = function(x) {if(x != 10 && x == 100) {document.write("<br />" + x * x * x);}}
+    document.write("<br />" + lk_1 + " " + lk_2(100));
+}
